99fe278bb3ccca67687ac85968166775c76abc25
kuzmin <kuz_alex_konst@mail.ru>
2024-03-17 10:20:18 +0400

    Simple Threads

---
+++ src/SimpleThread.java
@@ -0,0 +1,22 @@
+public class SimpleThread implements Runnable{
+
+    private final String str;
+
+    public SimpleThread(String str)
+    {
+        this.str = str;
+        (new Thread(this)).start();
+    }
+    @Override
+    public void run() {
+        try {
+            while(true) {
+                Thread.sleep(1000L);
+                System.out.println(this.str);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
