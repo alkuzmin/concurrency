ca63033270603537903ee2c9f974df9d26bb3f90
kuzmin <kuz_alex_konst@mail.ru>
2024-03-18 10:12:17 +0400

    MultiCasher

--- src/t1/PlayableInvHadler.java
+++ src/t1/PlayableInvHadler.java
@@ -7,16 +7,26 @@ import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class PlayableInvHadler implements InvocationHandler {
     private Object obj;
 
     PlayableInvHadler(Object obj) {
         this.obj = obj;
+        cashes = new HashMap<>();
+        modCounts = new HashMap<>();
     }
 
     private Object cashedVal;
     private Integer modCount=1;
+    private HashMap<String, Object> cashes;
+    private HashMap<String, Integer> modCounts;
+    private class Result
+    {
+        Object value;
+        Integer modCount;
+    }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -36,24 +46,26 @@ public class PlayableInvHadler implements InvocationHandler {
         }
 
         if (hasMutatorAnn) {
-
-            modCount++;
-            //cashedVal = method.invoke(obj, args);
+            //modCount++;
+            modCounts.put(m.getName(), modCounts.containsKey(m.getName())?modCounts.get(m.getName())+1:1);
             System.out.println("Not cashed val from mutator ");
             return method.invoke(obj, args);
 
         }
 
         if (hasCasheAnn) {
-            if (modCount > 0) {
+            if (!modCounts.containsKey(m.getName()) || modCounts.get(m.getName()) > 0) {
                 System.out.println("Not cashed val: ");
                 cashedVal = method.invoke(obj, args);
-                modCount=0;
+                cashes.put(m.getName(),cashedVal);
+                //modCount=0;
+                modCounts.put(m.getName(), 0);
                 return cashedVal;
             } else {
                 //modCount=0;
                 System.out.println("Cashed val: ");
-                return cashedVal;
+                //return cashedVal;
+                return cashes.get(m.getName());
             }
 
         }
